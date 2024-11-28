a08df311586d10eeea94076e2dd54d8c808382fb
Irash-Gilani <846852@pdsb.net>
2024-11-28 10:53:12 -0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,4 +1,7 @@
 import math
+
+
+
 class Point:
     def __init__(self, x, y):
         self.__x = x
@@ -11,19 +14,19 @@ class Point:
         return self.__y
     
     def getPoint(self):
-        return ({0}, {1}).format(self.__x, self.__y)
+        return "({0}, {1})".format(self.__x, self.__y)
    
     def distance(self, p2):
         return math.sqrt((self.__x-p2.x)**2 + (self.__y-p2.y)**2)
     
 class Polygon(Point):
     
-    def add_point(self, x, y):
+    def add_point(x, y):
         vertex = Point(x, y)
+        return vertex.getPoint()
         
-
 print(Polygon.add_point(5, 3))
         
         
-#unfinsihed 
-
+#unfinsihed
+# TODO Make a linked list with the points
