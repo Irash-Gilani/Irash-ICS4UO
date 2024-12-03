6ee9df85fde1c937dd6976a77c8ec93ba0287f4c
Irash-Gilani <846852@pdsb.net>
2024-12-03 09:32:55 -0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,49 +1,131 @@
-import math
 
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+# poly.py starts here
 
-class Point:
-    def __init__(self, x, y):
+class point:
+    def __init__(self, x: float=None, y: float=None):
+        # Default is None due to creation of a Head Node for linked lists
         self.__x = x
         self.__y = y
+        self.next = None
 
+
+    def valid(self):
+        # A validator is needed mostly for when we go to the end of the file, but
+        # also to assure us that the point is properly formatted with either
+        # ints or floats.
+        val = True
+        
+        if val == None:
+            val = False
+        
+        if val != int or val != float:
+            val = False
+    
+        return val
+    
     def getX(self):
         return self.__x
     
     def getY(self):
         return self.__y
     
-    def getPoint(self):
+    def __str__(self):
+        # point (x, y) expressed this way as string
+        # as in: (4, 5)
+        
         return "({0}, {1})".format(self.__x, self.__y)
-   
-    def distance(self, p2):
-        return math.sqrt((self.__x-p2.x)**2 + (self.__y-p2.y)**2)
-    
-class Polygon(Point):
-    
-    # creates a vertex
-    def add_point(x, y):
-        return Point(x, y).getPoint()
+
+class Polygon:
+    def __init__(self):
+        # Set basic properties to default values
+        self.__sides = 0
+        self.__vertices = 0
+        self.__head = point() # a null point with a null Next field
+        
+    def add_point(self, x: float, y: float):
+        # initialize a point object V
+        # if it is the first point, create the object with variable V make head.next point to V
+        # if it is any other point, V.next points to it, and V traverses to that point
+        # count the vertices as you go
+        
+        self.__x = x
+        self.__y = y
+        
+        if self.vertices == 0:
+            V = point(self.__x, self.__y)
+            self.__head.next = V
+            self.__vertices += 1
+        else:
+            V.next = V
+            self.__vertices += 1
+            V = V.next
+            self.__sides += 1
+            
+    def __str__(self):
+        # Use a traversal to generate the entire set of points separated by "->" as string
+        # You need to use point's __str__ function to help you.
+        
+        V = self.__head
+        
+        while V.next != None:
+            V.next = V
+            print(V, "-->", end="")
+            V = V.next
     
 
-try:
-    fh = open("a2.txt", "r")
-    
-    # read the line of coordinates, split them up and
-    # process them through a linked list
-    
-    fh.close()
+#poly.py ends here
 
-except OSError as err:
-    print("File not found", err)
-except EOFError as err2:
-    print("End of file exceeded", err2)
-    fh.close()
-  
-  
-  
-#unfinished
+# driver starts here
 
+# driver for the polygon class
+from poly import *
+
+def getNumeric(S : str):
+    # input: S is a point in the format "(x,y)" (type str)
+    # output: a tuple or list indicating a point (x, y) where x, y are int or float
+    
+    S = point(S)
+    
+    x = point.getX(S)
+    y = point.getY(S)
+
+    return (x, y)
+
+fh = open("a2.txt", "r") # this is the name of the data file to open
+
+polydata = fh.readline().strip()
+
+# make an array of points (str)
+
+polarr = []
+
+for x in polydata:
+    p = x.strip("()")
+    
+    [x, y] = p.split(",")
+    
+    try:
+        x = int(x)
+    except:
+        x = float(x)
+    
+    try:
+        y = int(y)
+    except:
+        y = float(y)
+    
+    pt = point(x, y)
+    
+    polarr.append(pt)
+
+# declare a polygon
+
+ Poly = polygon(points)
+
+# loop through the points array and turn them into numbers for the polynomial object
+    # generate an x, y pair (numerical not str) from getNumeric
+    # add to the polynomial (call add_point())
+
+print(Poly) # this should print the entire linked list of points as string
+
+# driver ends here
